9477248932da53139c272b903df2520dda794334
TgSkyline <tigomes.santos@gmail.com>
2023-01-14 17:51:19 -0300

    FEATURE (DataCience): Practice

---
+++ Practice.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+
+# series = [[23,45,12,679], [14,48,69,38]]
+# new_series = np.array(series)
+# print(new_series.ndim)
+# print(new_series.shape)
+
+
+# a= np.ones(10) + 4
+
+# a = np.ones(10) * 4
+
+# a = np.full(10, 4)
+
+# a = np.zeros(10) * 4
+
+# print(a)
+
+# import numpy as np
+# dataset = np.array(['paul', 'jacob', 'vince', 'paul', 'miky', 'larence', 'warren'])
+# print(dataset == 'paul')
+
+# import numpy as np
+
+# percentiles = [98, 76.37, 55.55, 69, 88]
+# first_subject = np.array(percentiles)
+# print(first_subject.dtype.name)
+
+# arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+# # arr[-2::-2] = -1
+
+# # arr[arr % 2 == 0] = -1
+
+# # arr[::2] = -1
+
+# arr[1::2] = -1
+
+# print (arr)
+
+
+# arr = np.eye(5)
+
+# print(arr)
+
+# # arr = arr - (np.eye(5) + 1)
+
+# # arr = np.full((5x5),-1)
+
+# arr[arr != 1] = -1
+
+# # arr[arr == 0] = -1
+
+# print(arr)
